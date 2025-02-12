f7757d5d3023461ccd89af93f103b012de9c2f19
Rakib Gazi <gazicourses@gmail.com>
2025-02-12 16:33:48 +0600

    added loading spiner in all post page

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -9,22 +9,28 @@ const AllPosts = () => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [sortOrder, setSortOrder] = useState("Recent");
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchFilteredPosts = async () => {
+      setLoading(true);
       try {
-        const response = await axios.post("https://ph-assignment-11-server-pink.vercel.app/volunteerPosts", {
-          title: search,
-          sortOrder: sortOrder || null, 
-        });
+        const response = await axios.post(
+          "https://ph-assignment-11-server-pink.vercel.app/volunteerPosts",
+          {
+            title: search,
+            sortOrder: sortOrder || null,
+          }
+        );
         setPosts(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching posts:", error);
       }
     };
-  
+
     fetchFilteredPosts();
   }, [search, sortOrder]);
-  
+
   return (
     <div className="container mx-auto px-4 pt-24">
       <div className="mt-12">
@@ -66,7 +72,7 @@ const AllPosts = () => {
               darkTheme ? "border-white" : "border-btm-footer"
             } select select-bordered w-full border-2`}
             value={sortOrder}
-            onChange={(e) => setSortOrder(e.target.value || "Recent")} 
+            onChange={(e) => setSortOrder(e.target.value || "Recent")}
           >
             <option value="Oldest">Recent Deadline</option>
             <option value="Recent">Oldest Deadline</option>
@@ -74,44 +80,55 @@ const AllPosts = () => {
         </div>
 
         <div className="divider divider-neutral"></div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6  xl:gap-6 2xl:gap-3 px-2 mb-20">
-          {posts.map((post) => (
-            <div
-              className={`card card-compact  shadow-xl ${
-                darkTheme ? " bg-active text-black" : "bg-base-100"
-              }`}
-              key={post._id}
-            >
-              <figure>
-                <img src={post.thumbnail} alt="Shoes" className="h-64 w-full" />
-              </figure>
-              <div className={`card-body `}>
-                <h2 className="card-title text-btm-footer">{post.title}</h2>
-                <p className="text-lg text-logo font-bold">
-                  Category:{" "}
-                  <span className="text-black font-normal text-base">
-                    {post.category}
-                  </span>
-                </p>
-                <p className="text-lg text-logo font-bold">
-                  Deadline:{" "}
-                  <span className="text-black font-normal text-base">
-                    {post.deadline}
-                  </span>
-                </p>
 
-                <div className="card-actions justify-end">
-                  <Link
-                    to={`/post/${post._id}`}
-                    className="btn bg-btm-footer hover:bg-active text-white hover:text-black font-bold w-full"
-                  >
-                    View Details
-                  </Link>
+        {loading ? (
+          <div className="flex justify-center py-10">
+            <span className="loading loading-spinner loading-lg text-primary"></span>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6  xl:gap-6 2xl:gap-3 px-2 mb-20">
+            {posts.map((post) => (
+              <div
+                className={`card card-compact  shadow-xl ${
+                  darkTheme ? " bg-active text-black" : "bg-base-100"
+                }`}
+                key={post._id}
+              >
+                <figure>
+                  <img
+                    src={post.thumbnail}
+                    alt="Shoes"
+                    className="h-64 w-full"
+                  />
+                </figure>
+                <div className={`card-body `}>
+                  <h2 className="card-title text-btm-footer">{post.title}</h2>
+                  <p className="text-lg text-logo font-bold">
+                    Category:{" "}
+                    <span className="text-black font-normal text-base">
+                      {post.category}
+                    </span>
+                  </p>
+                  <p className="text-lg text-logo font-bold">
+                    Deadline:{" "}
+                    <span className="text-black font-normal text-base">
+                      {post.deadline}
+                    </span>
+                  </p>
+
+                  <div className="card-actions justify-end">
+                    <Link
+                      to={`/post/${post._id}`}
+                      className="btn bg-btm-footer hover:bg-active text-white hover:text-black font-bold w-full"
+                    >
+                      View Details
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
